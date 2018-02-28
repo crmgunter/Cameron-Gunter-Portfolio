6dc92381c5c3fccb4df2974562b180809f6f1354
crmgunter <crmgunter@gmail.com>
2018-02-28 18:58:19 -0500

    starting working on landing screen

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+  $(document).ready(function() {
+	setTimeout(function() {
+		$("#animate").removeClass("is-loading");
+	}, 100)
+});
